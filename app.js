609931a37fb9a9956cde3d762d15176d5e8c15cd
Mutiulrasool <138636782+Mutiulrasool@users.noreply.github.com>
2023-10-14 22:24:05 -0700

    Add files via upload
    2nd Assignment is done.

---
+++ app.js
@@ -0,0 +1,32 @@
+var username = "Muti ul Rasool";
+var myName = "Muti ul Rasool";
+console.log(username + myName);
+alert(myName)
+
+var message = "Jhone Duck"
+console.log(message)
+alert(message)
+
+var message= "15 years old"
+console.log(message)
+alert(message)
+
+var message= "certified Mobile application development"
+console.log(message)
+alert(message)
+
+var message = "pizza\npizz\npiz\npi\n"
+console.log(message)
+alert(message)
+
+var email= "mrhajib@gmail.com"
+console.log(email)
+alert(email)
+
+var book= "A smarter way to learn java script"
+console.log(book)
+alert(book)
+
+var ability= "Yeah! I can write HTML Content through JS"
+console.log(ability)
+alert(ability)
